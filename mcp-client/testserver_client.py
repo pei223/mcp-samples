b8f918fc0a1f8d2c49e56720868eb02d1cebf5f6
pei <peidparc@gmail.com>
2025-05-29 19:35:14 +0900

    自前mcp-server & github-mcp-serverに接続するclientのテスト

--- mcp-client/testserver_client.py
+++ mcp-client/testserver_client.py
@@ -1,30 +1,74 @@
 import asyncio
+import os
+import pprint
 from fastmcp import Client
+from fastmcp.client.transports import StdioTransport
 
-async def log_mcp_server_info(client: Client):
+
+def format(t):
+    return pprint.pformat(t, indent=2)
+
+
+# resource/tool/promptといったmcpサーバー情報を出力
+async def log_mcp_server_info(client: Client, exclude_prompt=False):
     tools = await client.list_tools()
-    print(f"Available tools: {tools}\n")
-    
+    print(f"Available tools: {format(tools)}\n\n")
+
     static_resources = await client.list_resource_templates()
-    print(f"Available static resources: {static_resources}\n")
-    
+    print(f"Available static resources: {format(static_resources)}\n\n")
+
     dynamic_resources = await client.list_resource_templates()
-    print(f"Available dynamic resources: {dynamic_resources}\n")
+    print(f"Available dynamic resources: {format(dynamic_resources)}\\n")
 
-    static_prompts = await client.list_prompts()
-    print(f"Available static prompts: {static_prompts}\n")
+    if not exclude_prompt:
+        static_prompts = await client.list_prompts()
+        print(f"Available static prompts: {format(static_prompts)}\n\n")
 
-    dynamic_prompts = await client.list_prompts()
-    print(f"Available dynamic prompts: {dynamic_prompts}\n")
+        dynamic_prompts = await client.list_prompts()
+        print(f"Available dynamic prompts: {format(dynamic_prompts)}\\n")
 
 
 async def main():
-    # Connect via stdio to a local script
+    # http経由で自前のmcp-serverへの接続
     async with Client("http://localhost:8000/sse") as client:
         await log_mcp_server_info(client)
 
-        result = await client.call_tool("enc", {"enc_key": "test", "text": "text"})
-        print(f"Result: {result}\n")
+        folder_result = await client.call_tool(
+            "enc", {"enc_key": "test", "text": "text"}
+        )
+        print(f"Result: {folder_result}\n")
+
+    # github-mcp-serverを試す
+    async with Client(
+        StdioTransport(
+            # github-mcp-serverへのパス.
+            # 実行ファイルを直接指定で起動&接続してくれる.
+            "../../github-mcp-server/dist/github-mcp-server",
+            ["stdio"],
+            {
+                "GITHUB_PERSONAL_ACCESS_TOKEN": os.environ[
+                    "GITHUB_PERSONAL_ACCESS_TOKEN"
+                ]
+            },
+        )
+    ) as client:
+        await log_mcp_server_info(client, exclude_prompt=True)
+        # resourceを試しに実行してみる
+        folder_result = await client.read_resource(
+            "repo://pei223/clean-news/refs/heads/main/contents/batch"
+        )
+        print(f"\n\nResource result(folder): {folder_result}\n\n")
+        folder_result = await client.read_resource(
+            "repo://pei223/clean-news/refs/heads/main/contents/batch/Makefile"
+        )
+        print(f"Resource result(file): {folder_result}\n\n")
+
+        # toolを試しに実行してみる
+        tool_result = await client.call_tool(
+            "list_issues", {"owner": "pei223", "repo": "clean-news"}
+        )
+        print(f"Tool result: {tool_result}\n\n")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- mcp-server/testserver.py
+++ mcp-server/testserver.py
@@ -1,12 +1,16 @@
 # mcp_server.
+from typing import Annotated
 from fastmcp import FastMCP
+from pydantic import Field
 
 mcp = FastMCP("demo")
 
+
 # static resource
 @mcp.resource("config://version")
-def get_version(): 
+def get_version():
     return "2.0.1"
+
 
 # dynamic resource (dummy)
 @mcp.resource("accountbanks://date/{date}")
@@ -18,19 +22,43 @@ def get_profile(date: str) -> list[dict]:
             "detail": "supermarket",
             "price": 2000,
         },
-                {
+        {
             "date": "2025/05/21",
             "category": "food",
             "detail": "subscripion food service",
             "price": 15000,
-        }
+        },
     ]
+
+
+@mcp.tool(name="get_events_from_calender", description="レンダーから予定を取得する")
+async def get_events_from_calender(
+    from_date: Annotated[str, Field(description="取得したい予定の開始日")],
+    to_date: Annotated[str, Field(description="取得したい予定の終了日")],
+):
+    # ダミーデータ
+    return [
+        {"date": "2025/05/25", "title": "打合せ"},
+        {"date": "2025/05/28", "title": "歯医者"},
+        {"date": "2025/05/30", "title": "一人で買い物"},
+        {"date": "2025/05/31", "title": "飲み会"},
+    ]
+
+
+@mcp.tool(name="write_text_file", description="ファイルをテキストに保存")
+async def write_text_file(
+    file_name: Annotated[str, Field(description="保存ファイル名")],
+    text: Annotated[str, Field(description="保存したいテキスト")],
+):
+    with open(file_name, mode="w", encoding="utf-8") as file:
+        file.write(text)
+
 
 @mcp.tool(name="enc", description="暗号化")
 async def encrypt(enc_key: str, text: str) -> str:
     # でたらめな暗号化
-    return enc_key[::-1]+text[::-1]
-    
+    return enc_key[::-1] + text[::-1]
+
 
 if __name__ == "__main__":
     mcp.run(transport="sse")
